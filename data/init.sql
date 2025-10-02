c9c97e647917e071f048cfdfe5e0a780fb17ff40
Fery Wardiyanto <ferywardiyanto@gmail.com>
2025-10-02 12:02:54 +0700

    Upddate stack
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- data/init.sql
+++ data/init.sql
@@ -1,34 +1,34 @@
-CREATE TABLE IF NOT EXISTS public.attachments
-(
-  id character varying NOT NULL,
-  caption character varying,
-  filename character varying NOT NULL,
-  filetype character varying NOT NULL,
-  mimetype character varying NOT NULL,
+CREATE TABLE IF NOT EXISTS public.attachments (
+  "id" varchar NOT NULL,
+  "caption" varchar,
+  "filename" varchar NOT NULL,
+  "filetype" varchar NOT NULL,
+  "mimetype" varchar NOT NULL,
   "timestamp" timestamp with time zone NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS public.customers
-(
-  id uuid NOT NULL DEFAULT gen_random_uuid(),
-  phone character varying(15) NOT NULL,
-  name character varying(50),
-  address character varying(100),
+CREATE TABLE IF NOT EXISTS public.customers (
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "phone" varchar(15) NOT NULL,
+  "name" varchar(50),
+  "address" varchar(100),
   PRIMARY KEY (id),
   UNIQUE (phone)
 );
 
-CREATE TABLE IF NOT EXISTS public.chat_sessions
-(
-  id uuid NOT NULL DEFAULT gen_random_uuid(),
-  customer_id uuid references customers(id),
-  subject character varying(255),
-  started_at timestamp with time zone,
-  finished_at timestamp with time zone,
+CREATE TABLE IF NOT EXISTS public.chat_sessions (
+  "id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "customer_id" uuid references customers(id),
+  "subject" varchar(255),
+  "location" varchar(255),
+  "started_at" timestamp with time zone,
+  "finished_at" timestamp with time zone,
   PRIMARY KEY (id)
 );
 
 ALTER TABLE IF EXISTS public.attachments OWNER to automation;
 ALTER TABLE IF EXISTS public.customers OWNER to automation;
 ALTER TABLE IF EXISTS public.chat_sessions OWNER to automation;
+
+CREATE SCHEMA IF NOT EXISTS gowa;
